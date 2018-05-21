d757164c4727e5a60ac029f4e9c03b5b3d2b730d
akk0rd <vadim.riznik@gmail.com>
2018-05-21 12:31:30 -0400

    add demonstrative for acknowledge: 1) start brk_simple_recieve.py 2) start simple_recieve.py 3) start simple_sender.py
    expected:
    	brk_simple_receive.py get message and pause for 10 seconds,
    	after this it die, and give message simple_server.py

--- brk_simple_receive.py
+++ brk_simple_receive.py
@@ -14,7 +14,7 @@ print ' [*] Waiting for messages. To exit press CTRL+C'
 def callback(ch, method, properties, body):
     #sys.exit(0)
     print " [x] In recieve callback"
-    print " [x] Message: %r" %(body,)
+    time.sleep(10)
 #    response = 'Changed: ' + body
 
 #    channel = connection.channel()
@@ -22,6 +22,7 @@ def callback(ch, method, properties, body):
 #    channel.queue_declare(queue='insideDTMq', durable=True, arguments={'x-message-ttl' : 30000, "x-max-length":20})
 #    channel.basic_publish(exchange='', routing_key='insideDTMq', body=response, properties=pika.BasicProperties(delivery_mode = 2,))
 
+    sys.exit(0)
     ch.basic_ack(delivery_tag = method.delivery_tag)
     print " [x] out callback"
 
--- simple_sender.py
+++ simple_sender.py
@@ -5,25 +5,25 @@
 import pika
 import sys
 
-connection = pika.BlockingConnection(pika.ConnectionParameters('192.168.43.64'))
+connection = pika.BlockingConnection(pika.ConnectionParameters('127.0.0.1'))#'192.168.43.64'))
 channel = connection.channel()
 
-channel.queue_declare(queue='outsideDTM', durable=True, arguments={'x-message-ttl' : 1000, "x-max-length":2})
+channel.queue_declare(queue='outsideDTMq', durable=True, arguments={'x-message-ttl' : 30000, "x-max-length":20})
 
-channel.basic_publish(exchange='', routing_key='outsideDTM', body='Hello World!', properties=pika.BasicProperties(delivery_mode = 2,))
+channel.basic_publish(exchange='', routing_key='outsideDTMq', body='Hello World!', properties=pika.BasicProperties(delivery_mode = 2,))
 print " [x] Sent 'Hello World!'"
 
-channel = connection.channel()
-channel.queue_declare(queue='insideDTM', durable=True, arguments={'x-message-ttl' : 1000, "x-max-length":2})
+#channel = connection.channel()
+#channel.queue_declare(queue='insideDTM', durable=True, arguments={'x-message-ttl' : 1000, "x-max-length":2})
 
-def callback(ch, method, properties, body):
-    print " [x] In callback"
-    print " [x] Received %r" % (body)
+#def callback(ch, method, properties, body):
+#    print " [x] In callback"
+#    print " [x] Received %r" % (body)
+#
+#    ch.basic_ack(delivery_tag = method.delivery_tag)
+#    print "exit callback"
+#
+#channel.basic_consume(callback,
+#                      queue='insideDTM')
 
-    ch.basic_ack(delivery_tag = method.delivery_tag)
-    print "exit callback"
-
-channel.basic_consume(callback,
-                      queue='insideDTM')
-
-channel.start_consuming()
+#channel.start_consuming()
