bca6bb75caf789eac58a55173b77dcb197a64a12
akk0rd <vadim.riznik@gmail.com>
2018-05-21 10:12:20 -0400

    add numerating messages

--- htask/new_task.py
+++ htask/new_task.py
@@ -12,8 +12,8 @@ message = ' '.join(sys.argv[1:]) or "Hello World!"
 for i in range (1,15):
     channel.basic_publish(exchange='',
         routing_key='itask_queue',
-                      body=message,
+                      body=message+str(i),
     )
-    print " [x] Sent %r" % (message,)
+    print " [x] Sent %r" % (message+str(i),)
     time.sleep(1)
 connection.close()
